d8e9474786f723ff154cca343995c6dd987557d3
irii <irii@irii-desktop.(none)>
2013-05-03 00:17:31 +0200

    Fix building Modules symbol errors need to be fixed

--- net/compat-drivers/compat/compat-3.8.c
+++ net/compat-drivers/compat/compat-3.8.c
@@ -13,6 +13,7 @@
  */
 
 #include <linux/hid.h>
+#include <linux/hid.h>
 #include <linux/module.h>
 #include "hid-ids.h"
 #include <linux/netdevice.h>
@@ -265,6 +266,27 @@ static const struct hid_device_id hid_mouse_ignore_list[] = {
 	{ HID_USB_DEVICE(USB_VENDOR_ID_APPLE, USB_DEVICE_ID_APPLE_GEYSER1_TP_ONLY) },
 	{ }
 };
+
+static bool hid_match_one_id(struct hid_device *hdev,
+		const struct hid_device_id *id)
+{
+	return (id->bus == HID_BUS_ANY || id->bus == hdev->bus) &&
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,8,0))
+		(id->group == HID_GROUP_ANY || id->group == hdev->group) &&
+#endif
+		(id->vendor == HID_ANY_ID || id->vendor == hdev->vendor) &&
+		(id->product == HID_ANY_ID || id->product == hdev->product);
+}
+
+const struct hid_device_id *hid_match_id(struct hid_device *hdev,
+		const struct hid_device_id *id)
+{
+	for (; id->bus; id++)
+		if (hid_match_one_id(hdev, id))
+			return id;
+
+	return NULL;
+}
 
 bool hid_ignore(struct hid_device *hdev)
 {
